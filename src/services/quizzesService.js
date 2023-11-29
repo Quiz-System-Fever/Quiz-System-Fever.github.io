a3a99bf6fadf9fa47ee768686a0633cf70f8fd81
Todor Mladenov <toshko958@gmail.com>
2023-11-29 20:13:17 +0100

    Updated quizzesService

--- src/services/quizzesService.js
+++ src/services/quizzesService.js
@@ -1,5 +1,17 @@
-import { get } from "./api.js";
+import { get, post } from "./api.js";
 
 const baseUrl = 'https://parseapi.back4app.com/classes/Quizzes';
 
 export const getQuizzes = () => get(baseUrl);
+export const getQuizById = (quizId) => get(`${baseUrl}/${quizId}?include=owner`);
+
+export const createQuiz = async (quizData, userId) => {
+    const body = Object.assign({}, quizData, {
+        owner: {
+            __type: 'Pointer',
+            'className': '_User',
+            objectId: userId,
+        }
+    })
+    await post(baseUrl, body);
+};
