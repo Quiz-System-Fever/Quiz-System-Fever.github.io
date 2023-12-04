e67a9032ad4b8e04e7a9ae35613d9ecda1d1bdf9
Todor Mladenov <toshko958@gmail.com>
2023-12-04 21:33:20 +0100

    Added edit and cancel edit functionality

--- src/views/editorView/questions.js
+++ src/views/editorView/questions.js
@@ -1,9 +1,11 @@
 import { html, nothing, render } from "../../lib/lit-html.js";
 import { createAnswers, viewAnswerTemplate } from "./answers.js";
 
+const editorTemplate = (question, questionIndex, onCancel) => html`   
     <div class="layout">
         <div class="question-control">
             <button class="input submit action"><i class="fas fa-check-double"></i> Save</button>
+            <button @click=${onCancel} class="input submit action"><i class="fas fa-times"></i> Cancel</button>
         </div>
         <h3>Question ${questionIndex + 1}</h3>
     </div>
@@ -12,8 +14,10 @@ import { createAnswers, viewAnswerTemplate } from "./answers.js";
         ${createAnswers(question.answers, questionIndex, question.correctIndex)}
     </form>`;
 
+const viewTemplate = (question, questionIndex, onEdit) => html`
 <div class="layout">
     <div class="question-control">
+        <button @click=${onEdit} class="input submit action"><i class="fas fa-edit"></i> Edit</button>
         <button class="input submit action"><i class="fas fa-trash-alt"></i> Delete</button>
     </div>
     <h3>Question ${questionIndex + 1}</h3>
@@ -30,9 +34,17 @@ export function createQuestionView(question, questionIndex) {
     const element = document.createElement('article');
     element.className = 'editor-question';
 
+    render(viewTemplate(question, questionIndex, onEdit), element);
     return element;
 
-    function addAnswer() {
+    function onCancel() {
+        render(viewTemplate(question, questionIndex, onEdit), element);
+    }
 
+    function onEdit() {
+        render(editorTemplate(question, questionIndex, onCancel), element);
+    }
+
+    async function onDelete() {
     }
 }
