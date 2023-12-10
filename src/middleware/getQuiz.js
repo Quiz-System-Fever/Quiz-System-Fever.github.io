ee3d2b083e7db04604805013b2d39e0602eba0d4
Todor Mladenov <toshko958@gmail.com>
2023-12-10 21:11:50 +0100

    Updated the quiz functionality

--- src/middleware/getQuiz.js
+++ src/middleware/getQuiz.js
@@ -5,12 +5,18 @@ let state = {};
 
 export async function getQuiz(ctx, next) {
     const quizId = ctx.params.id;
+    ctx.clearState = clearState.bind(null, quizId);
     if (state[quizId] == undefined) {
         ctx.loader();
         state[quizId] = await getQuizById(quizId);
         state[quizId].questions = await getQuestionsByQuizId(quizId);
+        state[quizId].answers = state[quizId].questions.results.map(q => undefined);
     }
-
+    
     ctx.quiz = state[quizId];
     next();
+}
+
+function clearState(quizId) {
+    delete state[quizId]
 }
--- src/views/quizView/quiz.js
+++ src/views/quizView/quiz.js
@@ -1,36 +1,43 @@
 import { html, nothing } from "../../lib/lit-html.js";
+import { sendSolution } from "../../services/solutionsService.js";
 
-const quizTemplate = (quiz, questions, currentIndex, goBack) => html`
+const quizTemplate = (quiz, questions, answers, currentIndex, goBack, onNext, startOver, onSelect, onSubmit) => html`
 <section id="quiz">
 <header class="pad-large">
     <h1>Extensible Markup Language: Question ${currentIndex + 1} / ${quiz.questionCount}</h1>
     <nav class="layout q-control">
     <span class="block">Question ${currentIndex + 1}</span>
-    ${questions.map((q, i) => html`<a class="q-index q-current q-answered" href="/quiz/${quiz.objectId}?question=${i + 1}"></a>`)}
+    ${questions.map((q, i) => questionBoxTemplate(quiz.objectId, i, answers[i] != undefined, currentIndex == i))}
     </nav>
     </header>
     <div class="pad-large alt-page">
     <article class="question">
         <p class="q-text">${questions[currentIndex].text}</p>
-        <form>
+        <form @change=${onSelect}>
             ${questions.map((q, i) => questionTemplate(q, i, currentIndex == i))}
         </form>
         <nav class="q-control">
-            <span class="block">12 questions remaining</span>
+            <span class="block">${answers.filter(a => a == undefined).length} questions remaining</span>
             ${currentIndex > 0
-        ? html`<a class="action" href="/quiz/${quiz.objectId}?question=${currentIndex + 1}" @click=${goBack}><i class="fas fa-arrow-left"></i> Previous</a>`
+        ? html`<a class="action" href="/quiz/${quiz.objectId}?question=${currentIndex}" @click=${goBack}><i class="fas fa-arrow-left"></i> Previous</a>`
         : nothing}
-            <a class="action" href="javascript:void(0)" ><i class="fas fa-sync-alt"></i> Start over</a>
+            <a class="action" href="javascript:void(0)" @click=${startOver} ><i class="fas fa-sync-alt"></i> Start over</a>
             <div class="right-col">
             ${currentIndex < questions.length - 1
-        ? html`<a class="action" href="javascript:void(0)" >Next <i class="fas fa-arrow-right"></i></a>`
+        ? html`<a class="action" href="/quiz/${quiz.objectId}?question=${currentIndex + 2}" @click=${onNext} >Next <i class="fas fa-arrow-right"></i></a>`
         : nothing}
-                <a class="action" href=#>Submit answers</a>
+        ${(answers.filter(a => a == undefined).length == 0) ||
+        (currentIndex == questions.length - 1)
+        ? html`<a class="action" href="javascript:void(0)" @click=${onSubmit} >Submit answers</a>`
+        : nothing}
             </div>
         </nav>
     </article>
 </div>
 </section>`;
+
+const questionBoxTemplate = (quizId, index, isAnswered, isCurrent) => html`
+<a class="q-index ${isAnswered ? 'q-answered' : ''} ${isCurrent ? 'q-current' : ''}" href="/quiz/${quizId}?question=${index + 1}"></a>`
 
 const questionTemplate = (question, questionIndex, isCurrent) => html`
 <div style=${isCurrent ? "display:block" : "display:none"}>
@@ -45,12 +52,51 @@ ${answer}
 </label>`
 
 export async function quizView(ctx) {
+    const user = ctx.user();
     const currentIndex = Number(ctx.querystring.split('=')[1] || 1) - 1;
     const questions = ctx.quiz.questions.results;
-    ctx.render(quizTemplate(ctx.quiz, questions, currentIndex, goBack));
+    const answers = ctx.quiz.answers;
+    ctx.render(quizTemplate(ctx.quiz, questions, answers, currentIndex, goBack, onNext, startOver, onSelect, onSubmit));
 
     function goBack() {
-        const currentIndex = Number(ctx.querystring.split('=')[1]) - 2;
-        ctx.render(quizTemplate(ctx.quiz, currentIndex, goBack));
+        const currentIndex = Number(ctx.querystring.split('=')[1] || 1) - 1;
+        ctx.render(quizTemplate(ctx.quiz, questions, answers, currentIndex, goBack, onNext, startOver, onSelect, onSubmit));
+    }
+
+    function onNext() {
+        const currentIndex = Number(ctx.querystring.split('=')[1] || 1) - 1;
+        ctx.render(quizTemplate(ctx.quiz, questions, answers, currentIndex, goBack, onNext, startOver, onSelect, onSubmit));
+    }
+
+    function startOver() {
+        if (confirm('Are you sure you want to restart this quiz?')) {
+            ctx.clearState();
+            ctx.page.redirect(`/quiz/${ctx.params.id}`)
+        }
+    }
+
+    function onSelect(event) {
+        const questionIndex = Number(event.target.name.split('-')[1]);
+        answers[questionIndex] = Number(event.target.value);
+        ctx.render(quizTemplate(ctx.quiz, questions, answers, currentIndex, goBack, onNext, startOver, onSelect, onSubmit));
+    }
+
+    async function onSubmit() {
+        const unansweredQuestions = answers.filter(a => a == undefined).length;
+        let result = 0;
+        if (unansweredQuestions) {
+            if (!confirm(`Are you sure you want to submit? You have ${unansweredQuestions} unanswered questions left.`)) {
+                return;
+            }
+        }
+
+        for (let i = 0; i < questions.length; i++) {
+            result += Number(answers[i] == questions[i].correctIndex);
+
+        }
+
+        const quizCopy = Object.assign({}, ctx.quiz);
+        const solution = await sendSolution(ctx.quiz.objectId, user.objectId, { result, total: questions.length, quizCopy });
+        ctx.page.redirect(`/summary/${solution.objectId}`);
     }
 }
