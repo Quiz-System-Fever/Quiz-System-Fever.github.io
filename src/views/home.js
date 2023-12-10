2e6bf662f6dd46905076cc5f6f8da6bc7859b7d0
Todor Mladenov <toshko958@gmail.com>
2023-12-10 16:33:01 +0100

    Updated home page to display most recent quiz info

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,13 @@
 import { html, nothing } from "../lib/lit-html.js";
 import { getQuizzes } from "../services/quizzesService.js";
 
-const homeTemplate = (user) => html`
+const homeTemplate = (user, quizzes) => html`
 <section id="welcome">
 <div class="hero layout">
     <div class="splash right-col"><i class="fas fa-clipboard-list"></i></div>
     <div class="glass welcome">
         <h1>Welcome to Quiz Fever!</h1>
-        <p>Home to 157 quizzes in 12 topics. <a href="/browse">Browse all quizzes</a>.</p>
+        <p>Home to ${quizzes.length} quizzes in ${quizzes.uniqueTopics} topics. <a href="/browse">Browse all quizzes</a>.</p>
         ${user
         ? nothing
         : html`<a class="action cta" href="/login">Sign in to create a quiz</a>`}
@@ -15,7 +15,7 @@ const homeTemplate = (user) => html`
 </div>
 <div class="pad-large alt-page">
     <h2>Our most recent quiz:</h2>
-    <!--TODO Display most recent quizzes-->
+    ${quizTemplate(quizzes[quizzes.length - 1])}
     <div>
         <a class="action cta" href="/browse">Browse all quizzes</a>
     </div>
@@ -25,21 +25,30 @@ const homeTemplate = (user) => html`
 const quizTemplate = (quiz) => html`
 <article class="preview layout">
 <div class="right-col">
-    <a class="action cta" href="/quiz/:id">View Quiz</a>
+    <a class="action cta" href="/details/${quiz.objectId}">View Quiz</a>
 </div>
 <div class="left-col">
-    <h3>Extensible Markup Language</h3>
-    <span class="quiz-topic">Topic: Languages</span>
+    <h3>${quiz.title}</h3>
+    <span class="quiz-topic">Topic: ${quiz.topic}</span>
     <div class="quiz-meta">
-        <span>15 questions</span>
+        <span>${quiz.questionCount} questions</span>
         <span>|</span>
         <span>Taken 54 times</span>
     </div>
 </div>
 </article>`
 
-export function homeView(ctx) {
+export async function homeView(ctx) {
     ctx.loader();
     const user = ctx.user();
-    ctx.render(homeTemplate(user));
+    const quizzes = await getQuizzes();
+    const uniqueTopics = quizzes.results.reduce((a, q) => {
+        if (!a.includes(q.topic)) {
+            a.push(q.topic);
+        }
+        return a
+    }, []);
+    
+    quizzes.results.uniqueTopics = uniqueTopics.length;
+    ctx.render(homeTemplate(user, quizzes.results));
 }
