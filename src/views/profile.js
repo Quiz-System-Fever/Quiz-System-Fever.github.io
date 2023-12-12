5645b3aa95acd5446f09da24ab897ecc3cdc44ba
Todor Mladenov <toshko958@gmail.com>
2023-12-12 22:32:15 +0100

    Added functionality to display quiz solutions

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,7 +1,8 @@
 import { html } from "../lib/lit-html.js";
 import { deleteQuiz, getQuizByUserId } from "../services/quizzesService.js";
+import { getSolutionsByOwnerId } from "../services/solutionsService.js";
 
-const profileTemplate = (user, quizzes, onDelete) => html`<section id="profile">
+const profileTemplate = (user, quizzes, solutions, onDelete) => html`<section id="profile">
 <header class="pad-large">
     <h1>Profile Page</h1>
 </header>
@@ -13,12 +14,7 @@ const profileTemplate = (user, quizzes, onDelete) => html`<section id="profile">
         <h2>Your Quiz Results</h2>
         <table class="quiz-results">
             <tbody>
-                <tr class="results-row">
-                    <td class="cell-1">23. March 2021</td>
-                    <td class="cell-2"><a href="#">RISC Architecture</a></td>
-                    <td class="cell-3 s-correct">85%</td>
-                    <td class="cell-4 s-correct">12/15 correct answers</td>
-                </tr>
+            ${solutions.map(s => resultTemplate(s))}
             </tbody>
         </table>
     </article>
@@ -31,36 +27,53 @@ const profileTemplate = (user, quizzes, onDelete) => html`<section id="profile">
 </div>
 </section>`;
 
+const resultTemplate = (result) => html`
+<tr class="results-row">
+<td class="cell-1">${result.createdAt}</td>
+<td class="cell-2"><a href="/summary/${result.objectId}">RISC Architecture</a></td>
+<td class="cell-3 ${result.quizCopy.summary > result.quizCopy.questions.results.length / 2 ? 's-correct' : 's-incorrect'}">
+        ${((result.quizCopy.summary / result.quizCopy.questions.results.length) * 100).toFixed(0)}%</td >
+    <td class="cell-4 ${result.quizCopy.summary > result.quizCopy.questions.results.length / 2 ? 's-correct' : 's-incorrect'}">
+    ${result.quizCopy.summary}/${result.quizCopy.questions.results.length} correct answers</td>
+</tr > `;
+
 const quizTemplate = (quiz, onDelete) => html`
-<article class="preview layout">
-<div class="right-col">
-    <a class="action cta" href="/details/${quiz.objectId}">View Quiz</a>
-    <a class="action cta" href="/edit/${quiz.objectId}"><i class="fas fa-edit"></i></a>
-    <a class="action cta" href="javascript:void(0)" @click=${() => onDelete(quiz)}><i class="fas fa-trash-alt"></i></a>
-</div>
-<div class="left-col">
-    <h3><a class="quiz-title-link" href="/details/${quiz.objectId}">${quiz.title}</a></h3>
-    <span class="quiz-topic">Topic: ${quiz.topic}</span>
-    <div class="quiz-meta">
-        <span>${quiz.questionCount} questions</span>
-        <span>|</span>
-        <span>Taken 54 times</span>
+    < article class="preview layout" >
+        <div class="right-col">
+            <a class="action cta" href="/details/${quiz.objectId}">View Quiz</a>
+            <a class="action cta" href="/edit/${quiz.objectId}"><i class="fas fa-edit"></i></a>
+            <a class="action cta" href="javascript:void(0)" @click=${() => onDelete(quiz)}><i class="fas fa-trash-alt"></i></a>
+</div >
+    <div class="left-col">
+        <h3><a class="quiz-title-link" href="/details/${quiz.objectId}">${quiz.title}</a></h3>
+        <span class="quiz-topic">Topic: ${quiz.topic}</span>
+        <div class="quiz-meta">
+            <span>${quiz.questionCount} questions</span>
+            <span>|</span>
+            <span>Taken 54 times</span>
+        </div>
     </div>
-</div>
-</article>`
+</article > `
 
 export async function profileView(ctx) {
     ctx.loader();
     const user = ctx.user();
-    const quizzes = await getQuizByUserId(user.objectId)
-    ctx.render(profileTemplate(user, quizzes.results, onDelete));
+    const [solutions, quizzes] = await Promise.all([
+        getSolutionsByOwnerId(user.objectId),
+        getQuizByUserId(user.objectId)]);
+
+    solutions.results.map(s =>
+        s.quizCopy.summary = s.quizCopy.answers.reduce((a, cr, i) =>
+            a + Number(cr == s.quizCopy.questions.results[i].correctIndex), 0));
+
+    ctx.render(profileTemplate(user, quizzes.results, solutions.results, onDelete));
 
     async function onDelete(quiz) {
         if (confirm('Are you sure you want to delete this quiz?')) {
             await deleteQuiz(quiz.objectId);
             const quizIndex = quizzes.results.indexOf(quiz);
             quizzes.results.splice(quizIndex, 1);
-            ctx.render(profileTemplate(user, quizzes.results, onDelete));
+            ctx.render(profileTemplate(user, quizzes.results, solutions.results, onDelete));
         }
     }
 }
